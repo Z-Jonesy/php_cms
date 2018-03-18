e1cf0ae4bfb7c4efe21f351ed785d3110cf5d190
Jonesy <zoltan_bogdan@live.com>
2018-03-18 19:46:05 +0100

    v4-4146  added admin listing page with szerkesztes.php

--- admin/szerkesztes.php
+++ admin/szerkesztes.php
@@ -6,44 +6,41 @@ $sql = "SELECT id, alias, sorrend, menunev, tartalom, letrehozas, modositas, lei
 		ORDER BY sorrend ASC";
 $eredmeny =	mysqli_query($dbconn, $sql);
 
-$tablazat = "<table>
+$tablazat = "<p><a href=\"letrehozas.php\"><i class=\"material-icons\">add_box</i>Új oldal</a></p>
+ <table>
     <tr>
-        <th>ID</th>
         <th>Alias</th>
         <th>Sorrend</th>
         <th>Menunév</th>
         <th>Tartalom</th>
-        <th>Létrehozás</th>
-        <th>Módosítás</th>
-        <th>Leírás</th>
-        <th>Kulcsszavak</th>
         <th>Státusz</th>
+        <th>Művelet</th>
     </tr>\n";
+    
 	while ($sor = mysqli_fetch_assoc($eredmeny)) {
         $tablazat.= "<tr>
-        <td>{$sor['id']}</td>
+
         <td>{$sor['alias']}</td>
         <td>{$sor['sorrend']}</td>
         <td>{$sor['menunev']}</td>
-        <td>{$sor['tartalom']}</td>
-        <td>{$sor['letrehozas']}</td>
-        <td>{$sor['modositas']}</td>
-        <td>{$sor['leiras']}</td>
-        <td>{$sor['kulcsszavak']}</td>
+        <td>".substr(strip_tags($sor['tartalom']), 0, 100)."</td>
         <td>{$sor['statusz']}</td>
+        <td><a href=\"modositas.php?id={sor['id']} \"><i class=\"material-icons\">mode_edit</i></a>
+            <a href=\"torles.php?id={sor['id']} \"><i class=\"material-icons\">delete</i></a>
+          </td>
         </tr>\n";
     }
-$tablazat.= "</table>\n"
+$tablazat.= "</table><p><a href=\"letrehozas.php\"><i class=\"material-icons\">add_box</i>Új oldal</a></p>\n";
 
 // Sablonozó
 $sablon = file_get_contents("sablon.html");
-$sablon = str_replace("{{menu}}", $menu, $sablon);
-$sablon = str_replace("{{menunev}}", $menunev, $sablon);
-$sablon = str_replace("{{tartalom}}", $tartalom, $sablon);
-$sablon = str_replace("{{oldalsav}}", $oldalsav, $sablon);
-$sablon = str_replace("{{leiras}}", $leiras, $sablon);
-$sablon = str_replace("{{kulcsszavak}}", $kulcsszavak, $sablon);
-$sablon = str_replace("{{letrehozas}}", $letrehozas, $sablon);
+$sablon = str_replace("{{menu}}", "", $sablon);
+$sablon = str_replace("{{menunev}}", "szerkesztés", $sablon);
+$sablon = str_replace("{{tartalom}}", $tablazat, $sablon);
+$sablon = str_replace("{{oldalsav}}", " ", $sablon);
+
 
 print $sablon;
+
+
 ?>
